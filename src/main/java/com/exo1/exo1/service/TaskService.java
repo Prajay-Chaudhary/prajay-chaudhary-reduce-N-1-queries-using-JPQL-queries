05e349307611d1d94d18bc2c596752dabcebdbae
Prajay-Chaudhary <prajaychaudhary5678@gmail.com>
2024-11-05 22:52:01 +0100

    feat: pagination on task entity

--- src/main/java/com/exo1/exo1/service/TaskService.java
+++ src/main/java/com/exo1/exo1/service/TaskService.java
@@ -7,6 +7,8 @@ import com.exo1.exo1.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.webjars.NotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -16,8 +18,8 @@ public class TaskService {
     private TaskRepository taskRepository;
     private TaskMapper taskMapper;
 
-    public List<TaskDto> findAll() {
-        return taskMapper.toDtos(taskRepository.findAll());
+    public Page<TaskDto> findAll(Pageable pageable) {
+        return taskRepository.findAll(pageable).map(taskMapper::toDto);
     }
 
     public TaskDto findById(long id) {
